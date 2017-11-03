3dd9e9564005f9dd1e42729df8b866e145ffcb32
Rui Cao <rcao@iwu.edu>
2017-11-02 21:54:46 -0500

    Add files via upload

---
+++ pages/ticket/ticket.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ticket',
+  templateUrl: 'ticket.html'
+})
+export class ticketPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
