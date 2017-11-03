391afd507604d3924d3ad6d4d8808e950a2489f6
Rui Cao <rcao@iwu.edu>
2017-11-02 21:52:00 -0500

    Add files via upload

---
+++ pages/livechat/livechat.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-livechat',
+  templateUrl: 'livechat.html'
+})
+export class livechatPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
